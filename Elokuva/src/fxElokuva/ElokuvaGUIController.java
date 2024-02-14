c35fe61e2e1092d55c0a397c54dc8c05d89d3e52
Iljin Teemu Sakari <Teemu.iljin@gmail.com>
2024-02-14 11:33:50 +0200

    Linkattiin scene ja intellij

---
+++ Elokuva/src/fxElokuva/ElokuvaGUIController.java
@@ -0,0 +1,18 @@
+package fxElokuva;
+import javafx.fxml.FXML;
+import fi.jyu.mit.fxgui.ComboBoxChooser;
+import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ModalController;
+
+/**
+ * @author teemuiljin
+ * @version 13.2.2024
+ */
+public class ElokuvaGUIController {
+    // TODO
+    @FXML private void Nappula() {
+        Dialogs.showMessageDialog("Wuuuuu");
+    }
+
+}
+
--- Elokuva/src/fxElokuva/ElokuvaMain.java
+++ Elokuva/src/fxElokuva/ElokuvaMain.java
@@ -1,4 +1,4 @@
-package fxlista;
+package fxElokuva;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -9,19 +9,19 @@ import javafx.fxml.FXMLLoader;
 
 /**
  * @author teemuiljin
- * @version 24.1.2024
+ * @version 13.2.2024
  */
-public class listaMain extends Application {
+public class ElokuvaMain extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader ldr = new FXMLLoader(getClass().getResource("Elokuvalista.fxml"));
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("ElokuvaGUIView.fxml"));
             final Pane root = ldr.load();
-            //final listaGUIController listaCtrl = (listaGUIController)ldr.getController();
+            //final ElokuvaGUIController elokuvaCtrl = (ElokuvaGUIController)ldr.getController();
             Scene scene = new Scene(root);
-            scene.getStylesheets().add(getClass().getResource("lista.css").toExternalForm());
+            scene.getStylesheets().add(getClass().getResource("elokuva.css").toExternalForm());
             primaryStage.setScene(scene);
-            primaryStage.setTitle("lista");
+            primaryStage.setTitle("Elokuva");
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
