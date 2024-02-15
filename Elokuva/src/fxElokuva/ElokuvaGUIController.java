333a79a32a641d0faa1bf138c79ce65dcee39a0c
Iljin Teemu Sakari <Teemu.iljin@gmail.com>
2024-02-15 09:40:30 +0200

    Vaihe 3 tehty

--- Elokuva/src/fxElokuva/ElokuvaGUIController.java
+++ Elokuva/src/fxElokuva/ElokuvaGUIController.java
@@ -11,7 +11,22 @@ import fi.jyu.mit.fxgui.ModalController;
 public class ElokuvaGUIController {
     // TODO
     @FXML private void Nappula() {
-        Dialogs.showMessageDialog("Wuuuuu");
+        Dialogs.showMessageDialog("Pituus: 2h 35min, imdb: 8.2, Genre:  kauhu");
+    }
+    @FXML private void Asetukset() {
+        Dialogs.showMessageDialog("Help: lisää ja poista jäseniä painamalla lisää/poista jäseniä. Hae hakukentän avulla tiettyjä elokuvia tietokannasta. Paina genrejä nähdäksesi sen genren elokuvia");
+    }
+    @FXML private void Kauhu() {
+        Dialogs.showMessageDialog("Näyttää kauhuelokuvat");
+    }
+    @FXML private void Fantasia() {
+        Dialogs.showMessageDialog("Näyttää fantasiaelokuvat");
+    }
+    @FXML private void Toiminta() {
+        Dialogs.showMessageDialog("Näyttää toimintaelokuvat");
+    }
+    @FXML private void lisäys() {
+        Dialogs.showMessageDialog("Emme osaa lisätä tai poistaa elokuvia");
     }
 
 }
